8214462065a0672f893a269bd60800b467e3a723
Primas <primas.petrov@gmail.com>
2017-12-28 23:46:12 +0200

    init

---
+++ T.ConsoleClient/Stratup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using Т.Core;
+
+namespace T.ConsoleClient
+{
+    public class Stratup
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var image1 = @"C:\Users\Primas\Pictures\Saved Pictures\1.jpeg";
+            var image2 = @"C:\Users\Primas\Pictures\Saved Pictures\2.png";
+            var image3 = @"C:\Users\Primas\Pictures\Saved Pictures\3.png";
+
+            Console.WriteLine(new TextExtractorcs().ReadBg(image2));
+        }
+    }
+}
---
+++ Т.Core/TextExtractorcs.cs
@@ -0,0 +1,48 @@
+﻿using Tesseract;
+
+namespace Т.Core
+{
+    public class TextExtractorcs
+    {
+        public string ReadEng()
+        {
+            var text = string.Empty;
+            var path = @"C:\Users\Primas\Pictures\Saved Pictures\TesseractPicture.PNG";
+            var language = "eng";
+            var tessdata = @"./tessdata";
+
+            using (var engine = new TesseractEngine(tessdata, language, EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(path))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        text = page.GetText();
+                    }
+                }
+            }
+
+            return text;
+        }
+
+        public string ReadBg(string path = @"C:\Users\Primas\Pictures\Saved Pictures\TesseractPictureBg.PNG")
+        {
+            var text = string.Empty;
+            var language = "bul";
+            var tessdata = @"./tessdata";
+
+            using (var engine = new TesseractEngine(tessdata, language, EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(path))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        text = page.GetText();
+                    }
+                }
+            }
+
+            return text;
+        }
+    }
+}
